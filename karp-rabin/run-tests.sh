4914b00d127ac2dd9eac207e0dc723368c8a05b2
Gianluca Della Vedova <gianluca@dellavedova.org>
2014-11-05 08:14:50 +0100

    new tests for Karp-Rabin

--- karp-rabin/run-tests.sh
+++ karp-rabin/run-tests.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 dir="tests"
-test -f "$dir/chrI.fa.gz" || wget -O "$dir/chrI.fa.gz" http://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/chromosomes/chrI.fa.gz
-test -f "$dir/chr1.fa.gz" || wget -O "$dir/chr1.fa.gz" http://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr1.fa.gz
-for t in ${dir}/input/*
+test -f "$dir"/testo.1.fa.gz || wget -O "$dir"/testo.1.fa.gz http://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/chromosomes/chrI.fa.gz
+test -f "$dir"/testo.2.fa.gz || wget -O "$dir"/testo.2.fa.gz http://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr1.fa.gz
+
+for id in $(seq 2)
 do
-    p=$(cat "$t")
-    i=$(basename "$t")
-    ./karp-rabin --text "$dir"/chrI.fa.gz --pattern="$p" --rounds=5 | grep -oP "\d+" > "${dir}/output/$i".1
-    ./karp-rabin --text "$dir"/chr1.fa.gz --pattern="$p" --rounds=5 | grep -oP "\d+" > "${dir}/output/$i".2
+    text="${dir}/testo.${id}.fa.gz"
+    test -f "$text" || exit 1
+    for t in ${dir}/input/*
+    do
+	p=$(cat "$t")
+	i=$(basename "$t")
+	./karp-rabin --text="$text" --pattern="$p" --rounds=5 | grep -oP "\d+" | sort -n > "${dir}/output/$i".kr.${id}
+    done
 done
 diff -uaw --strip-trailing-cr --ignore-all-space "${dir}/output/" "${dir}/ok/"
